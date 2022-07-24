27a7fc289ddac81df2634b2dea6d7a73ba5d82da
Quynh Le <quynh.le@employmenthero.com>
2022-07-24 21:56:59 +0700

    Initial Commit

---
+++ Shared/Assignment_OneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_OneApp.swift
+//  Shared
+//
+//  Created by Le Helen on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_OneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
